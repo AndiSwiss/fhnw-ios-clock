4e92be745b9e12c37395c67c35f6b1baa6634299
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2021-11-17 22:08:51 +0100

    Initial Commit

---
+++ fhnw-ios-clock/fhnw_ios_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  fhnw_ios_clockApp.swift
+//  fhnw-ios-clock
+//
+//  Created by Andreas Ambühl on 17.11.21.
+//
+
+import SwiftUI
+
+@main
+struct fhnw_ios_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fhnw-ios-clockUITests/fhnw_ios_clockUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  fhnw_ios_clockUITestsLaunchTests.swift
+//  fhnw-ios-clockUITests
+//
+//  Created by Andreas Ambühl on 17.11.21.
+//
+
+import XCTest
+
+class fhnw_ios_clockUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
